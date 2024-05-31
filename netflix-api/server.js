88965fd15cfb78309245a46880ab83e834fb3321
Kanishka Raj Kaushik <kanishkaraj227@gmail.com>
2024-06-01 01:28:28 +0530

    add initial files to netflix-api

---
+++ netflix-api/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const User = require("./models/UserModel")
+const userRoutes = require("./routes/UserRoute")
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect("mongodb://localhost:27017/netflix")
+  .then(() => console.log("DB Connected"));
+
+app.use("/api/user",userRoutes)
+
+app.listen(5000, () => console.log("server started"));
